b2746f8ad0a4be4372d4656e71fbfb1ca6744a6e
srinivasp5647 <61374036+srinivasp5647@users.noreply.github.com>
2020-02-23 16:15:46 +0530

    Update three_sum.py
    Edited solution based on interviewbit solution

--- three_sum.py
+++ three_sum.py
@@ -1,24 +1,17 @@
-# This Function returns a number
-# Which is nearest to given target
-def three_sum(a, target):
-    result = []
-    for i in range(0, len(a)):                     # Loop will return all added values in a list
-        for j in range(1, len(a)):
-            for k in range(2, len(a)):
-                if (i != j and i != k and j != k):
-                    r = a[i] + a[j] + a[k]
-                    result.append(r)
-    diff = []
-    for i in range(0, len(result)):                # Loop will return the nearest value of the target
-        if result[i] > target:
-            diff.append(result[i] - target)
-        else:
-            diff.append(target - result[i])
-    index = diff.index(min(diff))
-    return result[index]
-
-user_input = [-1, 2, 1, -4]
-target = 1
-result = three_sum(user_input, target)
-# Below prints nearest value of the target
-print(result)
+class Solution:
+    def threeSumClosest(self, a, target):
+        result = []
+        for i in range(0, len(a)):
+            for j in range(1, len(a)):
+                for k in range(2, len(a)):
+                    if (i != j and i != k and j != k):
+                        r = a[i] + a[j] + a[k]
+                        result.append(r)
+        diff = []
+        for i in range(0, len(result)):
+            if result[i] > target:
+                diff.append(result[i] - target)
+            else:
+                diff.append(target - result[i])
+        index = diff.index(min(diff))
+        return result[index]
